baa9540ff14d6de031f15ce6fdefe76809cf9acf
Abdulmateen Tairu <tay2druh@gmail.com>
2019-11-12 15:25:54 +0100

    changed database engine

--- donaypay/settings.py
+++ donaypay/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'donaypay.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME':  os.path.join(os.getcwd(), 'db.sqlite3'),
     }
 }
 
